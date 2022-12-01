09e0b14afe89b1e4a55cad4c3c59c80653a1402b
Svec <anton.svec13@gmail.com>
2022-12-01 06:21:35 +0100

    day 1

---
+++ 01/aoc2022_01.py
@@ -0,0 +1,28 @@
+def max_calory(calories):
+    print("The maximum caloris: ", max(calories))
+
+def top_3_valories_sum(calories):
+    top3 = sorted(calories)[-3:]
+    print("The sum of top 3 calory: " + str(sum(top3)))
+
+
+
+def main():
+    with open('input.txt') as file:
+        lines = file.read()
+        calories = [0]
+        index = 0
+        for s in lines.split('\n'):
+            if s != '':
+                calory = int(s)
+                calories[index] += calory
+            else:
+                index += 1
+                calories.append(0)
+        print(calories)
+    ####################################
+    max_calory(calories)
+    top_3_valories_sum(calories)
+
+if(__name__ == "__main__"):
+    main()
